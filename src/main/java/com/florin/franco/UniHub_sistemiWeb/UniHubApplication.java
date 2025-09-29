5f8d0118d57a74e20afe0bf797e5828a960aadff
Francisc <franciscpurice@gmail.com>
2025-09-29 23:30:26 +0200

    firt commit

---
+++ src/main/java/com/florin/franco/UniHub_sistemiWeb/UniHubApplication.java
@@ -0,0 +1,13 @@
+package com.florin.franco.UniHub_sistemiWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florin/franco/UniHub_sistemiWeb/UniHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florin.franco.UniHub_sistemiWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
